ca2e26b6f52910e6805300c8cca0fa00c04fb357
franspaco <franspaco@gmail.com>
2017-09-04 11:58:11 -0500

    Finished

--- RegresionLogistica/RegresionLogistica/Proyecto4.py
+++ RegresionLogistica/RegresionLogistica/Proyecto4.py
@@ -12,7 +12,7 @@ def readFile(fileName):
         spamreader = csv.reader(csvfile, delimiter=',')
         for row in spamreader:
             xIn.append([])
-            #xIn[-1].append(float(1))
+            xIn[-1].append(float(1))
             xIn[-1].append(float(row[0])/100)
             xIn[-1].append(float(row[1])/100)
             yIn.append(int(row[2]))
@@ -21,15 +21,6 @@ def readFile(fileName):
     xIn = np.matrix(xIn)
     yIn = np.matrix(yIn).T
     return xIn, yIn
-
-def graficaDatos(X, Y, theta):
-    m = X.shape[0]
-    for n in range(0,m):
-        if bool(Y[n,0]):
-            plt.plot(X[n,0], X[n,1], 'rx')
-        else:
-            plt.plot(X[n,0], X[n,1], 'bo')
-    plt.show()
 
 def sigmoidal(z):
     return 1/(1+np.exp(-z))
@@ -40,13 +31,13 @@ def h(x, theta):
 def funcionCosto(theta, X, Y):
     m = X.shape[0]
     J = 0
-   # for i in range(0,m):
-       # y = Y[i,0]
-        #x = X[i]
-       # hip = h(x, theta)
-        #J += -y*np.log(hip)-((1-y)*np.log(1-hip))
 
-    #J /= m
+    hip = h(X, theta)
+    for i in range(0,m):
+        y = Y[i,0]
+        J += -y*np.log(hip[i,0])-((1-y)*np.log(1-hip[i,0]))
+
+    J /= m
 
     grad = X.T * (h(X, theta) - Y)
     grad /= m
@@ -57,26 +48,46 @@ def aprende(theta, X, Y, iteraciones):
     error = []
     for num in range(0, iteraciones):
         j, grad = funcionCosto(theta, X, Y)
-        #error.append(j.sum())
-        theta -= 0.1 * grad.A1
-        #print(str(num) + " " + str(j) + " " + str(theta))
+        error.append(j.sum())
+        theta -= 3 * grad.A1
+        print(str(num) + " " + str(j) + " " + str(theta))
     return theta, error
 
 def predice(theta, X):
     X = np.matrix(X) / 100
+    X = np.matrix(np.concatenate(([1], X.A1)))
     p = h(X,theta)
     if p >= 0.5:
         return 1
     else:
         return 0
 
+def graficaDatos(X, Y, theta):
+    m = X.shape[0]
+    for n in range(0,m):
+        if bool(Y[n,0]):
+            plt.plot(X[n,1]*100, X[n,2]*100, 'rx')
+        else:
+            plt.plot(X[n,1]*100, X[n,2]*100, 'bo')
+    rr = np.arange(20,100,1)
+    plt.plot(rr, f(rr, theta)*100, 'g')
+    plt.xlabel('Examen 1')
+    plt.ylabel('Examen 2')
+    plt.show()
+
+def f(x, theta):
+    return -theta[0]/theta[2]-(theta[1]/theta[2])*(x/100)
+    #return 1.25-1.1971*x
+
 if __name__ == '__main__':
     fileName = 'ex2data1.txt'
     X, Y = readFile(fileName)
-    #graficaDatos(X,Y, 1)
-    theta, error = aprende([0,0], X, Y, 1500)
+    theta, error = aprende([0,0,0], X, Y, 1500)
     print(theta)
-    print(h([45, 85], theta))
-    predice(theta, [45, 85])
     plt.plot(error)
+    plt.ylabel('Costo')
+    plt.xlabel('Iteracion')
     plt.show()
+    graficaDatos(X,Y, theta)
+    #print(h([1, 0.45, 0.85], theta))
+    print(predice(theta, [45, 85]))
