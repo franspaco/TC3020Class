c45ffea432e77554c08f7b35af23d6fd121f907d
franspaco <franspaco@gmail.com>
2017-10-30 15:30:59 -0600

    ni idea

--- Digits/Digits/Proyecto8.py
+++ Digits/Digits/Proyecto8.py
@@ -5,13 +5,19 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+from scipy.optimize import minimize
 
 
 def sig(z):
-    try:
-        return 1 / (1 + math.exp(-z))
-    except:
-        return 0
+    if z < 0:
+        return 1 - 1/(1 + math.exp(z))
+    else:
+        return 1/(1 + math.exp(-z))
+    #return 1 / (1 + math.exp(-z))
+    #try:
+        #return 1 / (1 + math.exp(-z))
+    #except:
+        #return 0
 
 vsig = np.vectorize(sig)
 
@@ -54,38 +60,39 @@ def ps(x):
         print(len(sub), end=" ")
     print("")
 
-def funCosto(h, y):
+def funCosto(h, y, W1, W2, lam):
     m = h.shape[1]
-    #J = (-(np.log(h).T * y) - (np.log(1-h).T * (1-y))).sum()
-    #J /= m
-    #J = 0
+
     J = - np.multiply(y, np.log(h)) - np.multiply(1-y, np.log(1-h))
     J = J.sum()
-    #for i in range(0,m):
-        #for k in range(0,h.shape[0]):
-            #J += -y[k, i] * np.log(h[k,i]) - (1-y[k, i])*np.log(1-h[k,i])
+    
     J /= m
-    return J
+
+    reg = (lam /(2.0*m)) * (np.power(W1[:,1:], 2).sum() + np.power(W2[:,1:], 2).sum())
+
+    return J + reg
 
 def entrenaRN(input_layer_size, hidden_layer_size, num_labels, X, y):
-    alpha = 0.003
+    alpha = 0.1
     #alpha = 0.0003
     #alpha = 0.00001
+
+    lam = 1
+
     m = X.shape[1]
 
     # X is 401xm
     X = np.concatenate((np.ones((1,X.shape[1])),X), axis=0)
-    A = []
-    A.append(X)
+    A0 = X
     # W1 is 25x401
-    W1 = randInicializacionPesos(401, 25)
+    W1 = randInicializacionPesos(input_layer_size + 1, hidden_layer_size)
     # W2 is 10x26
-    W2 = randInicializacionPesos(26, 10)
+    W2 = randInicializacionPesos(hidden_layer_size + 1, num_labels)
 
-    print('X:  ' + str(X.shape))
-    print('y:  ' + str(y.shape))
-    print('W1: ' + str(W1.shape))
-    print('W2: ' + str(W2.shape))
+    #print('X:  ' + str(X.shape))
+    #print('y:  ' + str(y.shape))
+    #print('W1: ' + str(W1.shape))
+    #print('W2: ' + str(W2.shape))
 
     costs = []
     running = True
@@ -96,42 +103,60 @@ def entrenaRN(input_layer_size, hidden_layer_size, num_labels, X, y):
         # Hidden layer
 
         # Z1 is 25xm
-        Z1 = W1 * A[0]
+        Z1 = W1 * A0
         # tempA is 25xm
-        tempA = vsig(Z1)
+        A1 = vsig(Z1)
         # tempA is 26xm
-        tempA = np.concatenate((np.ones((1,tempA.shape[1])),tempA), axis=0)
+        A1 = np.concatenate((np.ones((1,A1.shape[1])),A1), axis=0)
         # A1 is 26xm
-        A.append(tempA)
 
         # Output layer
 
         # Z2 is 10xm
-        Z2 = W2 * A[1]
+        Z2 = W2 * A1
         # A2 is 10xm
-        A.append(vsig(Z2))
+        A2 = vsig(Z2)
 
         # COST 
-        J = funCosto(A[-1], y)
+        J = funCosto(A2, y, W1, W2, 1)
         print('IT #' + str(count) + ' J=' + str(J), end='\r')
         costs.append(J)
         #print('J=  ' + str(J))
 
         # BACK PROPAGATION
 
+        # ESTO EN UNA COPIA LITERAL DE CADA COSA.. maybe works?
+        delta1 = np.zeros(W1.shape) 
+        delta2 = np.zeros(W2.shape)
+
+        for n in range(m):
+            d_2 = y[:,n] - A2[:,n]
+            d_1 = np.multiply(W2.T * d_2, sigmoidalGradienteA(A1[:,n]))
+            d_1 = d_1[1:]
+            delta2 += d_2 * A1[:,n].T
+            delta1 += d_1 * A0[:,n].T
+
+        W2grad =  alpha * (1/m) * delta2 + (lam/m) * np.concatenate((np.zeros((W2.shape[0],1)),W2[:,1:]), axis=1)
+        W1grad =  alpha * (1/m) * delta1 + (lam/m) * np.concatenate((np.zeros((W1.shape[0],1)),W1[:,1:]), axis=1)
+
+        W2 += W2grad
+        W1 += W1grad
+
+        # ESTO ES LO QUE ESTABA USANDO
         # 10xm = (10xm - 10xm) .* 10xm
-        d2 = np.multiply(A[-1] - y, sigmoidalGradienteA(A[2]))
-        d2 = A[-1] - y
+        #d2 = np.multiply(A2 - y, sigmoidalGradienteA(A[2]))
+        #d2 = A2 - y
+        # NO SE CUAL DE ESTAS DOS USAR
 
         # 26xm = (26x10 * 10xm) .* 26xm
-        d1 = np.multiply(W2.T * d2, sigmoidalGradienteA(A[1]))
+        #d1 = np.multiply(W2.T * d2, sigmoidalGradienteA(A1))
 
         # 25xm
-        d1 = d1[1:]
+        #d1 = d1[1:]
 
         # PARAM UPDATE
-        W2 -= alpha * d2 * A[1].T
-        W1 -= alpha * d1 * A[0].T
+        #W2 -= alpha * (1/m) * d2 * A1.T + (lam/m) * np.concatenate((np.zeros((W2.shape[0],1)),W2[:,1:]), axis=1)
+        #W1 -= alpha * (1/m) * d1 * A0.T + (lam/m) * np.concatenate((np.zeros((W1.shape[0],1)),W1[:,1:]), axis=1)
 
         if (len(costs) < 100) or deltaIsBig(costs[-1], costs[-2]):
             running = True
@@ -139,9 +164,10 @@ def entrenaRN(input_layer_size, hidden_layer_size, num_labels, X, y):
             print('\nSTOP by Delta')
             pass
         if( len(costs) > 100 and costs[-1] > costs[-2]):
-            running = False
-            print('\nSTOP by Bounce')
-        if count > 5000:
+            #running = False
+            #print('\nSTOP by Bounce')
+            pass
+        if count > 200:
             running = False
             print('\nSTOP by Iterations')
 
@@ -153,7 +179,8 @@ def deltaIsBig(current, last):
 
 
 def sigmoidalGradiente(z):
-    return np.multiply(vsig(z), 1-vsig(z))
+    A = vsig(z)
+    return np.multiply(A, 1-A)
 
 def sigmoidalGradienteA(A):
     return np.multiply(A, 1-A)
@@ -184,29 +211,13 @@ def prediceRNYaEntrenada(X,W1,W2):
     return max
 
 def drawImg(data, val = '?'):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, aspect='equal')
-    data = np.reshape(data, (20,20))
-
-    for x in range(0,20):
-        for y in range(0, 20):
-            color = (data[x,y]+1.0)/2.0
-            ax.add_patch(
-                patches.Rectangle(
-                    (float(x), float(19-y)), 1.0, 1.0,
-                    facecolor = (color, color, color)
-                )
-            )
-    axes = plt.gca()
-    axes.set_xlim([0,20])
-    axes.set_ylim([0,20])
+    plt.imshow(np.reshape(data, (20,20)),vmin=-1, vmax=1)
     plt.title('Drawing: ' + str(val))
-    plt.show()
+    plt.show();
 
 def training():
     print('\nREADING:')
-    #X, y = readFile('digitos5.txt')
-    X, y = readFile('digitos1000.txt')
+    X, y = readFile('digitos50.txt')
     print('\nSHAPES:')
     print(X.shape)
     print(y.shape)
@@ -215,15 +226,15 @@ def training():
     W1, W2, costs = entrenaRN(400, 25, 10, X, y)
     #print(W1)
     #print(W2)
-    np.save('w1_1000_5.npy', W1)
-    np.save('w2_1000_5.npy', W2)
+    np.save('w1_5000_1.npy', W1)
+    np.save('w2_5000_1.npy', W2)
 
     plt.plot(costs)
     plt.show()
     return W1, W2
 
 def predictions(W1, W2):
-    Xt, yt = readFile('digitos1000.txt')
+    Xt, yt = readFile('digitos50.txt')
     val = prediceRNYaEntrenada(Xt,W1,W2)
     total = Xt.shape[1]
     sucess = 0
@@ -241,8 +252,8 @@ def predictions(W1, W2):
 def main():
     W1, W2 = training()
 
-    #W1 = np.load('w1_1000_4.npy')
-    #W2 = np.load('w2_1000_4.npy')
+    #W1 = np.load('w1_5000_1.npy')
+    #W2 = np.load('w2_5000_1.npy')
 
     predictions(W1, W2)
 
